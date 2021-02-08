938607d7edaeff049f7fe72cfd4847f5f67da2ec
kgncsr <69785776+kgncsr@users.noreply.github.com>
2021-02-08 20:52:07 +0300

    a

--- ETicaret/Models/CategoryModel.cs
+++ ETicaret/Models/CategoryModel.cs
@@ -1,6 +1,7 @@
 ﻿using ETicaret.Entity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,8 @@ namespace ETicaret.WebUI.Models
     public class CategoryModel
     {
         public int CategoryId { get; set; }
+        [Required]
+        [StringLength(100,MinimumLength =5,ErrorMessage ="Kategori için 5-100 arasında değer giriniz")]
         public string Name { get; set; }
         public string Url { get; set; }
         public List<Product> Products { get; set; }
